1d058e816fd4c2a6c3a79a7467a10a44e41adf92
SKFaria <fariafaria737@gmail.com>
2021-09-02 14:32:31 +0600

    current

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+//console.log("button");
+
+searchBtn.addEventListener("click", function () {
+    const search = searchInput.value;
+    const url = "http://openlibrary.org/search.json?q=javascript";
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+});
+
